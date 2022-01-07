ed1065026ea0a37426809d49937148dbf771af2d
Charles Gabriel Lemes <charleslemes21@gmail.com>
2022-01-06 21:56:48 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Spline+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Portfólio</title>
+</head>
+<body>
+
+    <header>
+
+        <div class="center">
+        <div class="logo">PORTFÓLIO<span style="color: #2f1985">.</span></div><!--logo-->
+
+        <nav class="menu-desktop">
+            <ul>
+                <li><a href="">Tecnologias</a></li>
+                <li><a href="">Projetos</a></li>
+                <li><a href="">Sobre</a></li>
+            </ul>
+
+        </nav><!--menu-desktop-->
+
+        </div><!--center-->
+
+        <div class="clear"></div><!--clear-->
+
+        <nav class="menu-mobile">
+            <ul>
+                <li><a href="">Tecnologias</a></li>
+                <li><a href="">Projetos</a></li>
+                <li><a href="">Sobre</a></li>
+            </ul>
+
+        </nav><!--menu-mobile-->
+
+    </header>
+
+
+    <section class="main">
+        <div class="center">
+            <div class="conteudo-sobre">
+
+                <h2 class="dev">Desenvolvedor Front-end</h2>
+
+                <h2 class="title-autor"> Charles Gabriel Lemes</h2>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Dolores ducimus odio harum consectetur consequatur
+                    sapiente veritatis voluptatum ullam blanditiis at
+                </p>
+
+
+                <div class="button"><a target=_blank href="https://github.com/charleeslemes">GitHub</a></div><!--github-buttom-->
+
+            </div><!--conteudo-sobre-->
+
+
+            <div class="banner">
+                <div class="banner-top">
+                    <div class="img-html5"></div><!--html5-->
+                    <div class="img-css3"></div><!--html5-->
+                </div><!--banner-top-->
+
+                <div class="clear"></div><!--clear-->
+
+                <div class="banner-center">
+                        <div class="img-frente"></div><!--fundo-->
+                        <div class="img-fundo"></div><!--fundo-->
+                </div><!--banner-center-->
+
+                <div class="banner-bottom">
+                    <div class="img-js"></div><!--img-js-->
+
+                </div><!--banner-bottom-->
+
+
+            </div><!--banner-->
+
+            <div class="clear"></div>
+        </div><!--center-->
+    </section>
+
+
+
+    <section class="tec">
+        <div class="center">
+            <div class="wraper-title">
+                <div class=bullet></div><!--bullet-->
+                <div class="clear"></div>
+                    <div class="title">
+                        <h2>Tecnologias</h2>
+                    </div><!--title-->
+
+            </div><!--wraper-title-->
+
+        <div class="container-tec">
+            <div  class="tec-single">
+                <div style="background-image: url('images/logo-html.png'); "class="img-tec"></div><!--img-tec-->
+                 <p>HTML5</p>
+            </div><!--TEC-SINGLE-->
+
+            <div  class="tec-single">
+                <div style="background-image: url('images/logo-css.png'); " class="img-tec"></div><!--img-tec-->
+                 <p>CSS3</p>
+            </div><!--tec-single-->
+
+            <div   class="tec-single">
+                <div style="background-image: url('images/logo-js.png'); " class="img-tec"></div><!--img-tec-->
+                 <p>JAVASCRIPT</p>
+            </div><!--tec-single-->
+
+            <div   class="tec-single">
+                <div style="background-image: url('images/logo-jquery.png'); " class="img-tec"></div><!--img-tec-->
+                 <p>JQUERY</p>
+            </div><!--tec-single-->
+
+
+        </div><!--conteiner-tec-->
+    
+        </div><!--center-->
+    </section>
+
+    
+</body>
+</html>
