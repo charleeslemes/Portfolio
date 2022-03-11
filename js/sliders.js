c357f0d0ed408027bbc6470c6d022081480c4a8a
Charles Gabriel Lemes <charleslemes21@gmail.com>
2022-03-11 17:52:13 -0300

    fonte nova

---
+++ js/sliders.js
@@ -0,0 +1,23 @@
+$('.container-tec').slick({
+    dots: true,
+    arrows: false,
+    speed:1000,
+    slidesToShow:4,
+    slidesToScroll:1,
+    autoplay:true,
+    autoplaySpeed: 3000,
+    pauseOnHover:false,
+  
+});
+
+$('.container-diferenciais').slick({
+    dots: true,
+    arrows: false,
+    speed:1000,
+    slidesToShow:1,
+    slidesToScroll:1,
+    autoplay:true,
+    autoplaySpeed: 5000,
+    pauseOnHover:false,
+  
+});
