41a40cd2961b52b852ea3f04f86f75b4d55ba75b
Charles Gabriel Lemes <charleslemes21@gmail.com>
2022-03-14 12:03:39 -0300

    sessão de projetos

--- js/sliders.js
+++ js/sliders.js
@@ -10,14 +10,5 @@ $('.container-tec').slick({
   
 });
 
-$('.container-diferenciais').slick({
-    dots: true,
-    arrows: false,
-    speed:1000,
-    slidesToShow:1,
-    slidesToScroll:1,
-    autoplay:true,
-    autoplaySpeed: 5000,
-    pauseOnHover:false,
-  
-});
+
+
