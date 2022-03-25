b4f0bb24373ed641b321f981624ccf1eb890b124
Charles Gabriel Lemes <charleslemes21@gmail.com>
2022-03-25 11:15:31 -0300

    organizando código

--- js/functions.js
+++ js/functions.js
@@ -13,6 +13,35 @@ $(function(){
 
 
 
+     /* descrição autor */
+ScrollReveal({ reset: true});
+
+ScrollReveal().reveal('.conteudo-sobre .dev', { delay: 180});
+
+ScrollReveal().reveal('.conteudo-sobre .title-autor', { delay: 500});
+ScrollReveal().reveal('.conteudo-sobre .texto-autor ', { delay: 700});
+
+
+
+
+
+     function escritaAuto(elemento) {
+        const textoArray = elemento.innerHTML.split('');
+        textoArray.pop();
+        elemento.innerHTML = '';
+        textoArray.forEach((letra, i) => {
+           setTimeout(() => elemento.innerHTML = elemento.innerHTML.replace("|", "") + letra +"|", 120 * i);
+           if(textoArray.length-1 == i){
+              setTimeout(() => escritaAuto(elemento), 75 * i + 20000);
+           }
+        });
+    
+        
+        
+     }
+     
+     const titulo = document.querySelector('.dev');
+     escritaAuto(titulo);
      
 
   
