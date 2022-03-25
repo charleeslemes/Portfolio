8cd230af5b20e9f74651c2eb73244ee022710085
Charles Gabriel Lemes <charleslemes21@gmail.com>
2022-03-24 22:37:16 -0300

    efeito add

--- js/functions.js
+++ js/functions.js
@@ -10,59 +10,18 @@ $(function(){
           mostrarMenu.slideToggle();
       })
      }
+
+
+
+     
+
   
   })
 
 
-  
-  /*usando vanilla */
-
-
-    var btnRight = document.querySelector('section.projetos .arrow-right');
-    var imgsSlider = document.querySelectorAll('section.projetos .projetos-single');
-   
-    
-    var btnLeftVoltar = 0;
-
-
-
-    var lastIndex=0;
-
-    imgsSlider.forEach((itens,index)=>{
-   
-    document.querySelectorAll('section.projetos .bullet-single')[index]
-    .addEventListener('click',()=>{
-       
-           
-           
-                var lastImage  = document.querySelectorAll('section.projetos .projetos-single')[lastIndex];
-                var actualImage = document.querySelectorAll('section.projetos .projetos-single')[index];
-
-
-                document.querySelectorAll('.bullet-single')[lastIndex]
-                .classList.remove('active-bullet');
-        
-        
-                ;
-                document.querySelectorAll('.bullet-single')[index]
-                .classList.add('active-bullet');
-
-
-                lastImage.style.opacity =0;
-                actualImage.style.opacity = 1;
-        
-                lastIndex = index;
-
-         
-                
-            })
-
-
            
 
-      
-        
-        })
+
 
 
    
--- js/sliders.js
+++ js/sliders.js
@@ -12,3 +12,10 @@ $('.container-tec').slick({
 
 
 
+/* descrição autor */
+ScrollReveal({ reset: true});
+
+ScrollReveal().reveal('.conteudo-sobre .dev', { delay: 300});
+
+ScrollReveal().reveal('.conteudo-sobre .title-autor', { delay: 500});
+ScrollReveal().reveal('.conteudo-sobre .texto-autor ', { delay: 700});
