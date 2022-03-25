4be66bd957193cb463d468ed7ad41bdb323730ae
Charles Gabriel Lemes <charleslemes21@gmail.com>
2022-03-25 10:38:58 -0300

    efeito de escrita titulo dev

--- js/sliders.js
+++ js/sliders.js
@@ -19,3 +19,24 @@ ScrollReveal().reveal('.conteudo-sobre .dev', { delay: 300});
 
 ScrollReveal().reveal('.conteudo-sobre .title-autor', { delay: 500});
 ScrollReveal().reveal('.conteudo-sobre .texto-autor ', { delay: 700});
+
+
+
+
+function escritaAuto(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    textoArray.pop();
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+       setTimeout(() => elemento.innerHTML = elemento.innerHTML.replace("|", "") + letra +"|", 120 * i);
+       if(textoArray.length-1 == i){
+          setTimeout(() => escritaAuto(elemento), 75 * i + 5000);
+       }
+    });
+
+    
+    
+ }
+ 
+ const titulo = document.querySelector('.dev');
+ escritaAuto(titulo);
